36b85ae39d9e7c3d0b9badbe2ebf3a796dbe17ab
robin <sonirobin369@gmail.com>
2020-09-20 20:38:46 +0530

    adding the state to the burgerBuilder component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,13 +2,13 @@ import React from 'react'
 import Classes from './Burger.module.css'
 import BurgerIngredients from './BurgerIngredients/BurgerIngredients'
 const burger = (props) => {
+    const ingredientType = Object.keys(props.type).map((ind) => {
+       return [...Array(props.type[ind])].map(() => <BurgerIngredients type={ind} />) 
+    })
+    console.log(ingredientType)
     return(
         <div className ={Classes.Burger}>
-            <BurgerIngredients type="BreadTop" />
-            <BurgerIngredients type="Seeds" />
-            <BurgerIngredients type="Cheese" />
-            <BurgerIngredients type="Meat" />
-            <BurgerIngredients type="BreadBottom" />
+            {ingredientType}
         </div>
     )
 }
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react'
 import Auxiliary from '../../hoc/Auxiliary';
 import Burger from  '../../components/Burger/Burger'
 class BurgerBuilder extends Component{
+    state = {
+        ingredients : {
+            BreadTop:1,
+            Cheese:2,
+            Meat:1,
+            Salad:1,
+            Bacon:1,
+            BreadBottom:1
+        }
+    }
     render(){
         return(
             <Auxiliary>
-                <Burger />
+                <Burger type = {this.state.ingredients}/>
                 <div>Build Controls</div>
             </Auxiliary>
         );
