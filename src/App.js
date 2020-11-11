8500a6c5d0db9ba65da8e768751210657b876c00
robin <sonirobin369@gmail.com>
2020-11-11 09:49:13 +0530

    adding the Orders page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { Route, Switch } from 'react-router-dom';
 import BurgerIngredients from './components/Burger/BurgerIngredients/BurgerIngredients';
 import Layout from './components/Layouts/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
-import Checkout from './containers/Checkout/Checkout'
+import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders'
 class App extends Component {
   render(){
     return (
@@ -11,6 +12,7 @@ class App extends Component {
         <Layout>
           <Switch>
             <Route path = "/checkout" component = {Checkout}/>
+            <Route path = "/Orders" component = {Orders}/>
             <Route path = "/" exact component = {BurgerBuilder}/>
           </Switch>
           
